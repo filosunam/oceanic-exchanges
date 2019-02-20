3c69d7f9b3150c411fa264683bffebcd470ccce2
Marco Godínez <markotom@gmail.com>
2019-02-20 04:08:41 -0600

    improve performance when loading w2v models

--- api/routers/word-embedding/similarity.js
+++ api/routers/word-embedding/similarity.js
@@ -4,22 +4,28 @@ const w2v = require('word2vec');
 const w2vSimilarity = require('tasks/nlp/w2v-similarity');
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
 const { modelsPath } = require('config/w2v');
 const modelFiles = fs.readdirSync(modelsPath);
 
+w2v.loadModelAsync = promisify(w2v.loadModel);
+
 const models = [];
-for (let modelFile of modelFiles) {
-  if (path.extname(modelFile) === '.bin') {
-    w2v.loadModel(`${modelsPath}/${modelFile}`, function(err, model) {
-      if (!err) {
-        models.push({
-          name: modelFile.replace('.bin', ''),
-          model,
-        });
-      }
-    });
+
+async function loadModels() {
+  for (let modelFile of modelFiles) {
+    if (path.extname(modelFile) === '.bin') {
+      const model = await w2v.loadModelAsync(`${modelsPath}/${modelFile}`);
+      models.push({
+        name: modelFile.replace('.bin', ''),
+        model,
+      });
+      console.log('w2v model loaded =>', modelFile);
+    }
   }
 }
+
+loadModels();
 
 module.exports = new Route({
   method: 'get',
