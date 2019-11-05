652ad04ec32b46eabd58f8e9ea7257fd8aae30a9
Marco Godínez <markotom@gmail.com>
2019-11-04 23:34:07 -0600

    Add docker config

---
+++ api.dockerfile
@@ -0,0 +1,17 @@
+FROM node:11-alpine
+RUN apk add python gcc g++ make curl
+
+ARG WORKING_DIR=/var/www
+
+RUN mkdir -p $WORKING_DIR
+WORKDIR $WORKING_DIR
+
+COPY package.json $WORKING_DIR
+RUN yarn install
+
+COPY . $WORKING_DIR/
+
+RUN yarn global add pm2
+RUN pm2 install pm2-logrotate
+
+CMD ["pm2-runtime", "ecosystem.config.js", "--env", "production", "--only", "api"]
---
+++ ecosystem.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  apps : [{
+    name: 'api',
+    script: './api/runner.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }, {
+    name: 'app',
+    script: './app/runner.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }, {
+    name: 'admin',
+    script: './admin/runner.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+};
